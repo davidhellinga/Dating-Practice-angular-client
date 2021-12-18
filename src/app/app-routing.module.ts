3dab263c36d8beb8e686ae68f673a99a2aa68c88
David Hellinga <d.hellinga@student.fontys.nl>
2021-12-18 11:31:57 +0100

    End of section 7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,9 @@ import {MemberListComponent} from "./members/member-list/member-list.component";
 import {MemberDetailComponent} from "./members/member-detail/member-detail.component";
 import {MessagesComponent} from "./messages/messages.component";
 import {AuthGuard} from "./_guards/auth.guard";
+import {TestErrorsComponent} from "./errors/test-errors/test-errors.component";
+import {NotFoundComponent} from "./errors/not-found/not-found.component";
+import {ServerErrorComponent} from "./errors/server-error/server-error.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -17,9 +20,12 @@ const routes: Routes = [
       {path: 'members/:id', component: MemberDetailComponent},
       {path: 'lists', component: MemberListComponent},
       {path: 'messages', component: MessagesComponent},
-      {path: '**', redirectTo:'/', pathMatch: 'full'}
     ]
   },
+  {path:'errors', component:TestErrorsComponent},
+  {path: 'not-found', component:NotFoundComponent},
+  {path: 'server-error', component:ServerErrorComponent},
+  {path: '**', redirectTo:'not-found', pathMatch: 'full'}
 ];
 
 @NgModule({
