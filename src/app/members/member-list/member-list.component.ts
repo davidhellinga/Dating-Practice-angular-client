eff756c2198153462980d59dcc9823a946567357
David Hellinga <d.hellinga@student.fontys.nl>
2022-01-19 11:08:53 +0100

    End of section 13.

--- src/app/members/member-list/member-list.component.ts
+++ src/app/members/member-list/member-list.component.ts
@@ -1,7 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Member} from "../../_models/member";
 import {MembersService} from "../../_services/members.service";
-import {Observable} from "rxjs";
+import {Pagination} from "../../_models/pagination";
+import {UserParams} from "../../_models/userParams";
+import {User} from "../../_models/user";
 
 @Component({
   selector: 'app-member-list',
@@ -9,13 +11,40 @@ import {Observable} from "rxjs";
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-  members$: Observable<Member[]>;
+  members: Member[];
+  pagination: Pagination;
+  userParams: UserParams;
+  user: User;
+  sexualityList = [{value: "straight", display: "Straight"},
+    {value: "gay", display: "Gay"},
+    {value: "bi", display: "Bi"},
+    {value: "ace", display: "Ace"}]
 
   constructor(private memberService: MembersService) {
+    this.userParams=this.memberService.getUserParams();
   }
 
   ngOnInit(): void {
-    this.members$=this.memberService.getMembers();
+    this.loadMembers();
   }
 
+  loadMembers() {
+
+    this.memberService.setUserParams(this.userParams);
+    this.memberService.getMembers(this.userParams).subscribe(response => {
+      this.members = response.result;
+      this.pagination = response.pagination;
+    })
+  }
+
+  resetFilters(){
+    this.userParams=this.memberService.resetUserParams();
+    this.loadMembers();
+  }
+
+  pageChanged(event: any) {
+    this.userParams.pageNumber = event.page;
+    this.memberService.setUserParams(this.userParams);
+    this.loadMembers();
+  }
 }
